dd82aa28a440d1418b173b6413cb804a1148c112
abdomsaid <abdomsaid24@gmail.com>
2026-06-29 22:26:20 +0300

    -Added auth signUp functionalities

--- src/features/authentication/components/FormInput.tsx
+++ src/features/authentication/components/FormInput.tsx
@@ -1,3 +1,4 @@
+import { cva } from "class-variance-authority";
 import { type LucideIcon } from "lucide-react";
 
 export default function FormInput({
@@ -5,30 +6,53 @@ export default function FormInput({
   Icon,
   type,
   placeholder,
+  errorMSG,
+  ...props
 }: {
   title: string;
   Icon: LucideIcon;
   type?: string;
   placeholder: string;
+  errorMSG?: string;
 }) {
+  const classVariants = cva(
+    "rounded-xl border-2 px-5 py-2 relative z-0 flex w-full ",
+    {
+      variants: {
+        class: {
+          error: "border-red-600",
+          noError: "border-ring",
+        },
+      },
+      defaultVariants: { class: "noError" },
+    }
+  );
   return (
-    <div className="rounded-xl border-2 border-ring  px-5 py-2 relative z-0 flex w-full  ">
-      <div className="absolute z-1 -top-3.5 bg-gray-100">
-        <label className="text-xs"> {title}</label>
-      </div>
-      <div className="flex items-center gap-3  w-full">
-        <Icon
-          size={20}
-          className="color-ring"
-          strokeWidth={2}
-          absoluteStrokeWidth
-        />
+    <div>
+      {errorMSG && (
+        <p className="text-red-500 w-full text-center mb-2">{errorMSG}</p>
+      )}
 
-        <input
-          className="w-full outline-none"
-          placeholder={placeholder}
-          type={type}
-        />
+      <div className={classVariants({ class: errorMSG ? "error" : "noError" })}>
+        <label className="text-xs absolute z-1 -top-2.5 bg-gray-100 ">
+          {title}
+        </label>
+
+        <div className="flex items-center gap-3 w-full">
+          <Icon
+            size={20}
+            className="color-ring"
+            strokeWidth={2}
+            absoluteStrokeWidth
+          />
+
+          <input
+            {...props}
+            className="w-full outline-none"
+            placeholder={placeholder}
+            type={type}
+          />
+        </div>
       </div>
     </div>
   );
